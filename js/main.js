9280490f12321cccf3d90b8aa874644119c0264f
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-08-04 12:58:13 -0700

    added boilerplate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
 console.log('sanity check');
+
+
+/*----- constants -----*/ 
+
+
+/*----- app's state (variables) -----*/ 
+
+
+/*----- cached element references -----*/ 
+
+
+/*----- event listeners -----*/ 
+let click;
+
+/*----- functions -----*/
+init();
+
+
+/*Create 4x4 Flexbox 
+Fill 2 random cells with value of 2
+*/
+function init() {
+
+}
+
+
+function makeMove(click) {
+
+}
+
+function render() {
+
+}
+
+function checkForMatchRow() {
+
+}
+
+function checkForMatchColumn() {
+
+}
+
+function addAndDelete() {
+
+}
+
+function addNewNumber() {
+
+}
+
+function checkGameOver() {
+
+}
