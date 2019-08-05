6a449c72b02be81a24bb120a8a6c04f4eaeffe3f
Gregory Redig <gregoryredig@Gregorys-MacBook-Pro.local>
2019-08-05 15:22:49 -0700

    add css styling and set up framework for js

--- js/main.js
+++ js/main.js
@@ -5,24 +5,42 @@ console.log('sanity check');
 
 
 /*----- app's state (variables) -----*/ 
-
+    var board, score, movesMade;
 
 /*----- cached element references -----*/ 
 
 
 /*----- event listeners -----*/ 
-let click;
+
+    //LEFT ARROW '37'
+// document.onkeydown();
+//     //UP ARROW '38'
+// document.onkeydown();
+//     //RIGHT ARROW '39'
+// document.onkeydown();
+//     //DOWN ARROW '40'
+// document.onkeydown();
+
+
 
 /*----- functions -----*/
-init();
 
 
 /*Create 4x4 Flexbox 
 Fill 2 random cells with value of 2
 */
 function init() {
-
+    board = [
+        [0,0,0,0], // ROW 1
+        [0,0,0,0], // ROW 2
+        [0,0,0,0], // ROW 3
+        [0,0,0,0], // ROW 4
+    ];
+    movesMade = 0;
+    score = 0;
+    render();
 }
+
 
 
 function makeMove(click) {
@@ -30,6 +48,9 @@ function makeMove(click) {
 }
 
 function render() {
+    board.array.forEach(function(sq, idx) {
+
+    });
 
 }
 
