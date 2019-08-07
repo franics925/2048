84b668a4b027f62afb028117dc699b2d39866141
Gregory Redig <gregoryredig@Gregorys-MacBook-Pro.local>
2019-08-07 14:04:24 -0700

    added background color functionality to render

--- js/main.js
+++ js/main.js
@@ -4,29 +4,28 @@ console.log('sanity check');
 /*----- constants -----*/ 
 
 
+
+
 /*----- app's state (variables) -----*/ 
     var board, score, movesMade;
 
 /*----- cached element references -----*/ 
-    var emptySet;
+    var emptySet = [];
+    let direction;
 
 /*----- event listeners -----*/ 
     document.onkeydown = function(event) {
         switch (event.keyCode) {
         case 37:
-                //handle left click
                 moveLeft();
             break;
         case 38:
-                //handle up click
                 moveUp()
             break;
         case 39:
-                //handle right click
                 moveRight();
             break;
         case 40:
-                //handle down click
                 moveDown();
             break;
         }
@@ -56,43 +55,88 @@ init();
         render();
     }
 
+    function render() {
+        board.forEach(function(rowArr, rowIdx) {
+            rowArr.forEach(function(cell, colIdx) {
+                let div = document.getElementById(`r${rowIdx}c${colIdx}`);
+                div.style.backgroundColor = 'yellow';
+
+                console.log(`rowArr: ${rowArr} ; rowIdx: ${rowIdx} ; cell: ${cell} ;  colIdx: ${colIdx}`);
+                console.log(div.id);
+
+
+        // $('board').each(function(idx) {
+        //     console.log( idx + ': ' + this.innerHTML );
+        //     });
+
+                // div.checkForMatchColumn();
+                // div.checkForMatchRow();
+            });
+
+        });
+    }
     function moveLeft(click) {
         console.log('Left key pressed');
+        direction = 'left';
+        movesMade += 1;
         //push all non empty tiles into next available open cell starting with left most value
+        board.forEach(function(rowArr, rowIdx) {
+            rowArr.forEach(function(cell, colIdx) {
+                
+            });
+        });
+
     }
     
     function moveUp(click) {
         console.log('Up key pressed');
+        direction = 'up';
+        movesMade += 1;
         //push all non empty tiles into next available open cell starting with top most value
+        board.forEach(function(rowArr, rowIdx) {
+            rowArr.forEach(function(cell, colIdx) {
+            });
+        });
+
     }
     
     function moveRight(click) {
         console.log('Right key pressed');
+        direction = 'right';
+        movesMade ++;
         //push all non empty tiles into next available open cell starting with right most value
+        board.forEach(function(rowArr, rowIdx) {
+            rowArr.forEach(function(cell, colIdx) {
+            });
+        });
+
     }
     
     function moveDown(click) {
         console.log('Down key pressed');
+        direction = 'down';
+        movesMade += 1;
         //push all non empty tiles down into next available open cell starting with bottom most value
-    }
-    
-
-    
-    function render() {
         board.forEach(function(rowArr, rowIdx) {
             rowArr.forEach(function(cell, colIdx) {
-                let marker = document.getElementById(`r${rowIdx}c${colIdx}`);
-                console.log(`rowArr: ${rowArr} ; rowIdx: ${rowIdx} ; cell: ${cell} ;  colIdx: ${colIdx}`);
-                console.log(marker);
-
-
-                // marker.checkForMatchColumn();
-                // marker.checkForMatchRow();
             });
-
         });
 
     }
+
+    function genRadomTwo() {
+        // generates two intergers '2' in two random cells on the board
+        board[Math.floor(Math.random() * 3)][Math.floor(Math.random() * 3)] = 2
+        console.log(board);
+        board.forEach(function(rowArr, rowIdx) {
+            rowArr.forEach(function(cell, colIdx) {
+                console.log(`r${rowIdx}c${colIdx}`)
+            });
+        });
+
+    }
+    
+
 
     function checkForMatchRow() {
 
@@ -104,6 +148,7 @@ init();
 
     function addAndDelete() {
 
+
     }
 
     function addNewNumber() {
@@ -113,3 +158,16 @@ init();
     function checkGameOver() {
 
     }
+
+
+    //     // the "document ready" approach 
+    // $(document).ready(function() {
+    //     // all of your app's js goes within this function
+    //     alert("Everything is ready, let's do this");
+    // });
+    
+    // // or, the shortcut version
+    // $(function(){
+    //     // all of your app's js goes within this function
+    //     alert("Everything is ready, let's do this");
+    // });
